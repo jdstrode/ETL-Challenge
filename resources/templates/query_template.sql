120d48064c1d2983d102c9fc1820143af6b0849c
Daniel Atuesta <danielatuesta55@gmail.com>
2021-05-08 12:35:27 -0500

    Final work book

--- resources/templates/query_template.sql
+++ resources/templates/query_template.sql
@@ -1,16 +1,13 @@
--- Query to check successful load
-SELECT * FROM population;
-SELECT * FROM world_happiness;
-
+-- Checking data was properly loaded
+SELECT * 
+FROM population;
+SELECT * 
+FROM world_happiness;
 -- Join tables on county_id
-SELECT world_happiness.id, world_happiness.Country_name, world_happiness.Ranking, population.id, population.Country_name, population.Total_Population, population.Population_Density
+CREATE TABLE merged AS  
+SELECT world_happiness.id, world_happiness.country_name, world_happiness.ranking, population.total_population, population.population_density
 FROM world_happiness
-INNER JOIN population
+JOIN population  
 ON world_happiness.id = population.id;
 
--- Creating the view
-CREATE VIEW AS world_happiness_vs_population
-SELECT world_happiness.id, world_happiness.Country_name, world_happiness.Ranking, population.Country_name, population.Total_Population, population.Population_Density
-FROM world_happiness
-INNER JOIN population
-ON world_happiness.id = population.id;
+SELECT * FROM merged;
